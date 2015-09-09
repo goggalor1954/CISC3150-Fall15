522f7b20ac547a4c8427a48829e969ec869a9535
goggalor1954 <goggalor1954@gmail.com>
2015-09-09 15:43:08 -0400

    I did something wrong with the last commit

--- HW2/Question1/PyramidBuild.java
+++ HW2/Question1/PyramidBuild.java
@@ -19,25 +19,30 @@ public class PyramidBuild{
 		}
 		return-1;
 	}
-	
+	// the idea here is to first create the string for the particular line, then figure out what you want the spaceing to be, then print out the line with the correct spaceing using printf. i could either create a new sring each time or midify an existing string. creating a new one seams easier. try creating the correct string first then work on the formating. this works. i would loe to figure out how to numbert oend up in the right side of the prompt so i get a better looking triangle.
+	//it looks like i need to create 2 strings, one for the everything befroe and one for everthing after the main number
 	void printPry(int numVal){
-		String pryStack;
-		for(int i=1; i<numVal; i++){
+		String pyrStack;
+		String width="%"+numVal+"s %n";
+		//this loop generates each layer of the pyramid
+		for(int i=1; i<=numVal; i++){
 			//need to use nested loop here, dont want to, should thingk of another way to do this
-			pryStack="";
+			pyrStack="";
+			//this nested loop generates the "bricks", ie numbers in the current layer.
 			for(int j=1; j<i*2; j++){
 				if(j<=i && i !=1){
-					pryStack=pryStack+j;
+					pyrStack=pyrStack+j;
 				}
 				else{
-					pryStack=pryStack+(i*2-j);
+					pyrStack=pyrStack+(i*2-j);
 				}
 			}
-			System.out.printf(%%numVal-i, %n, pryStack);
+		//	pyrStack=pyrStack+"\n";
+		//	print formating is only working for the first line for some reason
+			System.out.printf(width,pyrStack);
 			// still dont quit understant how to format printf corectly
 		}
 	}
-
 
 
 
