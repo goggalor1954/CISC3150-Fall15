8435ff19344db711bb788328062a167a03e50075
Jim <Goggalor1954@gmail.com>
2015-09-16 20:07:37 -0400

    Question 1 mostly works, still have some trouble with the spaceing of double digit numbers, working on trying to fix that. i i can't ill just release it as it.

--- HW2/Question1/PyramidBuild.java
+++ HW2/Question1/PyramidBuild.java
@@ -20,17 +20,35 @@ public class PyramidBuild{
 		return-1;
 	}
 // generate a front stack and a back stack based on the entered text, still dosent work with number >9
+// im going to rework this so that it prints out blank spaces in a loop untill it gets to the number.
 	void printPry(int numVal){
-		String pyrFrontStack="", pyrBackStack="\n";
-		String formating="%"+(numVal*2)+"s";
-		//this loop generates each layer of the pyramid
-		for(int i=1; i<=numVal; i++){
-			//asignes new value to the back for front stack, prints out front and back stack, adds the cuvent value of i to the front of the back stack.
-			pyrFrontStack=pyrFrontStack+i+" ";
-			System.out.printf(formating,pyrFrontStack);
-			System.out.print(pyrBackStack);
-			// still dont quit understant how to format printf corectly
-			pyrBackStack=i+" "+pyrBackStack;
+		//atempt to fix the spaceing issue with double digits;
+		String smallBrick="1", largeBrick+"2";
+		String brickSize=smallBrick;
+		//if(numVal>10) rowCount>=2+"";
+		//loop to print each row, starts out printing spaces, then moves to an inner loop that prints the numbers.
+		for(int rowCount=1; rowCount<=numVal; rowCount++){
+			//if(rowCount>10) brickSize=2+"";
+			for(int blankSpace=0; blankSpace<(numVal-rowCount)*2; blankSpace++){
+				System.out.printf("%"+brickSize+"s", "");
+			}
+			if(rowCount==1){
+				System.out.printf("%"+brickSize+"s", "");
+				System.out.printf("%"+brickSize+"d", rowCount);
+			}
+			else{
+			//prints out the front row
+				for(int frontPyrBrick=1; frontPyrBrick<=rowCount; frontPyrBrick++){
+					System.out.printf("%"+brickSize+"d", frontPyrBrick);
+					System.out.printf("%"+brickSize+"s", "");
+				}
+			//prints out the back of the row, so long as it is not the top of the pyramid
+				for(int backPryBrick=rowCount; backPryBrick>0; backPryBrick--){
+					System.out.printf("%"+brickSize+"d", backPryBrick);
+					System.out.printf("%"+brickSize+"s", "");
+				}
+			}
+			System.out.printf("%1s", "\n");
 		}
 	}
 
