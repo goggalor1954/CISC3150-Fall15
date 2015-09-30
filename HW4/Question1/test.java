4ff0f2cf55730c34d9e86cb7f12cb8908a37dd68
Jim <Goggalor1954@gmail.com>
2015-09-30 17:54:08 -0400

    Finished most of the functions for MyString. I only have toString and valueOf left to do.

--- HW4/Question1/test.java
+++ HW4/Question1/test.java
@@ -9,20 +9,32 @@ public class test {
 	}
 
 	void go(){
-		char[] testChars = {'d', 'o', 'n', 'n', 'a'};
+		char[] testChars = {'D', 'O', 'n', 'N', 'a'};
+		//char[] otherTestChars={'K','e','l','S','O'};
+		//char[] otherTestChars={'D', 'O', 'n', 'N'};
 		MyString testString = new MyString(testChars);
-
+		//MyString otherTestString = new MyString(otherTestChars);
 
 		//System.out.println(testString.length());
-		MyString testSubstring=testString.substring(1, 3);
-		printChars(testSubstring);
-	
+		//MyString testSubstring=testString.substring(1, 3);
+
+		//printChars(testString.toUpperCase());
+		//System.out.println(testString.equals(otherTestString));
+
+		char[] otherTestChar=testString.getMyString();
+		printArr(otherTestChar);
 	}	
 	void printChars( MyString newMyString){
 		if( newMyString.getTail()==null) return;
 
 		System.out.println(newMyString.getHead());
 		printChars(newMyString.getTail());
+	}
+	void printArr( char[] charArr){
+		for(int i=0; i< charArr.length; i++){
+			System.out.printf("%c",charArr[i]);
+		}
+		System.out.printf("\n");
 	}
 }
 
